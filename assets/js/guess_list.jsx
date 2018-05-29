a9a9b394e444e7ce7c682d2137f0685cd5af4af9
matt-clement <clement.matthewp@gmail.com>
2018-05-28 22:10:21 -0400

    Extract function into stateless component

--- assets/js/guess_list.jsx
+++ assets/js/guess_list.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import _ from 'lodash';
 
+const previousGuesses = (guesses) => {
+  return (
+    <ul id="previous-guesses" className="guess-list">
+      {_.map(guesses, (guessData) => {
+        let guessWord = guessData.word;
+        let likeness = guessData.likeness;
+        let entry = likeness == guessWord.length ? "Entry accepted." : "Entry denied."
+        let numberCorrect = likeness + "/" + guessWord.length + " correct."
+        return <li key={guessWord}>{guessWord}<br />{entry}<br />{numberCorrect}</li>
+      })}
+    </ul>
+  );
+};
+
 class GuessList extends React.Component {
   constructor(props) {
     super(props);
-    this.guessList = this.guessList.bind(this);
-  }
-
-  guessList() {
-    return (
-      <ul id="previous-guesses" className="guess-list">
-        {_.map(this.props.guesses, (guessData) => {
-          let guessWord = guessData.word;
-          let likeness = guessData.likeness;
-          let entry = likeness == guessWord.length ? "Entry accepted." : "Entry denied."
-          let numberCorrect = likeness + "/" + guessWord.length + " correct."
-          return <li key={guessWord}>{guessWord}<br />{entry}<br />{numberCorrect}</li>
-        })}
-      </ul>
-    );
   }
 
   render() {
     return (
       <div id="guess-list">
-        {this.guessList()}
+        {previousGuesses(this.props.guesses)}
         <ul id="new-entry-list">
           <li id="new-entry" key="new-entry">{this.props.hovered}</li>
         </ul>
